43c577e33ce9ff018b29be439049a64d5c35d8d7
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-06-13 10:09:44 -0700

    added questions

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 //require
+// const fs = require('fs');
+const inquirer = require('inquirer');
+//path
+const Employees = require('./lib/employees')
+const Engineer = require('./lib/engineer');
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern');
 
-//questions go here 
+const questions = [
+    //
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        message: 'What is your id?',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },//link to email
+    {
+        type: 'input',
+        message: 'What is your positon?',
+        name: 'position',
+    },
+];
+
+// function writeToFile(filename, data) {
+//     return fs.writeFileSync(path.join(process.cwd(), filename), data)
+// }
+
+// function init() {
+//     inquirer.prompt(questions).then((responses)=>{
+//         writeToFile('index.html', generateHTML({...responses}))
+//     })
+// }
+
+//init();
